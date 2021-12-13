9d3a3f478e9c0b5148e9f2c424545f006dc7f416
Michaël BUGNONE <bugnone.michael@gmail.com>
2021-12-13 20:02:14 +0100

    wip

--- poly_forum/lib/screens/home/home_screen.dart
+++ poly_forum/lib/screens/home/home_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:poly_forum/constants.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const route = "/Home";
 
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +30,29 @@ class HomeScreen extends StatelessWidget {
           Navigator.pop(context);
           return Future(() => true);
         },
-        child: const Scaffold(
+        child: Scaffold(
+          extendBody: true,
           backgroundColor: kScaffoldColor,
+          body: IndexedStack(
+            index: _selectedIndex,
+            children: <Widget>[
+              Container(
+                width: 290,
+                height: 210,
+                color: Colors.green,
+              ),
+              Container(
+                width: 250,
+                height: 170,
+                color: Colors.red,
+              ),
+              Container(
+                width: 220,
+                height: 150,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
         ));
-
-    // return Scaffold(
-    //   backgroundColor: kScaffoldColor,
-    // );
   }
 }
