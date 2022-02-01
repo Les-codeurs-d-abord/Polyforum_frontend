f30667828140941a9cc1cd3a378e8c37b1d43085
Justin <grosjean.justin.68@gmail.com>
2022-02-01 10:00:33 +0100

    fix: voeux candidats et entreprises

--- poly_forum/lib/screens/candidate/offers/components/add_wishlist_btn.dart
+++ poly_forum/lib/screens/candidate/offers/components/add_wishlist_btn.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poly_forum/cubit/candidate/candidate_choices_cubit.dart';
-import 'package:poly_forum/cubit/candidate/candidate_offer_screen_cubit.dart';
 import 'package:poly_forum/cubit/candidate/wishlist/candidate_get_wishlist_cubit.dart';
 import 'package:poly_forum/cubit/candidate/wishlist/candidate_wishlist_cubit.dart';
 import 'package:poly_forum/data/models/candidate_user_model.dart';
 import 'package:poly_forum/data/models/offer_model.dart';
 import 'package:poly_forum/screens/shared/components/row_btn.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poly_forum/utils/constants.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
